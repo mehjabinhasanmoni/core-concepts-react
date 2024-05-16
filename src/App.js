cd3b1508ae7e635b2e66a2a8b866e731bf117303
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2024-05-16 20:26:05 +0600

    number increase and decrease using state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const products = [
-    {name : 'Laptop', price: 50000},
-    {name : 'Mobile', price: 20000},
-    {name : 'Mouse', price: 1500},
-  ]
+ 
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price} warrenty='2 Years'></Product>)
-      }
-
-      {/* <Product name="Laptop" price="50000"></Product>
-      <Product name="Mobile" price="20000"></Product>
-      <Product name="Mouse" price="1500"></Product>
-       */}
-
-       {/* ------------------------- */}
-
+      <Counter></Counter>
 
     </div>
   );
 }
 
 
+function Counter(){
+  const [count, setCount] = useState(1);
+  const increaseCount = () =>{
+    const newCount = count + 1;
+    setCount(newCount);
+  }
 
-function Product(props) {
-  console.log(props);
+  const decreaseCount = () =>{
+    const newCount = count - 1;
+    setCount(newCount);
+  }
+
+
+  // console.log(abc)
   return(
-    <div className='product'>
-      <h2>Name : {props.name} </h2>
-      <p>Price : {props.price}</p>
-      <p>Warrenty : {props.warrenty}</p>
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
-  );
+  )
 }
+
+
+
+
+
+
+
+// ------------------------------------------------------
+
+ // const products = [
+  //   {name : 'Laptop', price: 50000},
+  //   {name : 'Mobile', price: 20000},
+  //   {name : 'Mouse', price: 1500},
+  // ]
+
+
+
+  //  {/* {
+  //       products.map(product => <Product name={product.name} price={product.price} warrenty='2 Years'></Product>)
+  //     } */}
+
+  //     {/* <Product name="Laptop" price="50000"></Product>
+  //     <Product name="Mobile" price="20000"></Product>
+  //     <Product name="Mouse" price="1500"></Product>
+  //      */}
+
+  //      {/* ------------------------- */}
+
+
+  // function Product(props) {
+  //   console.log(props);
+  //   return(
+  //     <div className='product'>
+  //       <h2>Name : {props.name} </h2>
+  //       <p>Price : {props.price}</p>
+  //       <p>Warrenty : {props.warrenty}</p>
+  //     </div>
+  //   );
+  // }
+
 
 export default App;
